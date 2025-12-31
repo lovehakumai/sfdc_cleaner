ef7b23ff1ee0e989cbb6d3b13e6f4ed0bbc1a1cc
lovehakumai <masaotomeza@gmail.com>
2026-01-01 01:39:12 +0900

    test push

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from config import config
-import streamlit as st
 from layout import main_screen, header
 
 def main():
-# --- 1. 設定の取得 ---
-    session, DB_SCHEMA = config()
-# --- 画面表示 ---
     header()
-    main_screen(session, DB_SCHEMA)
+    config()
+    main_screen()
 
 if __name__ == "__main__":
     main()
